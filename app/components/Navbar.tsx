d8b22dd7c3e90518aec036763a3c971095235f80
Josefin Ahlenius <josefinlouahlenius@gmail.com>
2024-12-23 12:51:13 +0100

    Small animation on hover on nav bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,14 +8,13 @@ const Navbar = () => {
 
     return (
         <nav className="flex flex-row justify-between p-4 fixed bottom-0 left-0 right-0 w-full bg-offWhite">
-            <Link href="/">Learn</Link>
+            <Link href="/" className="hover:scale-105 transition duration-300 ease-in-out">Learn</Link>
             {currentUser && (
                 <>
-                    <Link href="/practice">Practice</Link>
-                    <Link href="/profile">Profile</Link>
+                    <Link href="/practice" className="hover:scale-105 transition duration-300 ease-in-out">Practice</Link>
+                    <Link href="/profile" className="hover:scale-105 transition duration-300 ease-in-out">Profile</Link>
                 </>
             )}
-
         </nav>
     )
 }
